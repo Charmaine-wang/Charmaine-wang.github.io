4cef1d05451944d5500646d36f8f09fb33b89532
charmaine wang <charmainewang424@hotmail.com>
2020-04-20 11:07:28 +0200

    new portfolio

--- src/App.js
+++ src/App.js
@@ -1,44 +1,43 @@
 import React, { useState } from 'react';
-import FrontSection from './pages/FrontSection'
-import SecondSection from './pages/SecondSection'
-import Modal from './components/Modal'
+import FrontSection from './pages/FrontSection';
+import SecondSection from './pages/SecondSection';
+import Modal from './components/Modal';
 import './App.css';
 import styled from 'styled-components';
 
 const StyledWrapper = styled.div`
-  top: 100%;
-  z-index: 999;
-  right: 0;
-  margin: 0 10px;
-  /* justify-content: center; */
-  position: fixed;
-  text-align: center;
-  transform: rotate(90deg);
-  white-space: nowrap;
-  h3 {
-      padding: 2px 4px;
-      margin: 0;
-      position: absolute;
-      right: 0;
-      background-color: #0000FF;
-      color: #fff;
-
-  } 
-`
+	top: 100%;
+	z-index: 999;
+	right: 0;
+	margin: 0 10px;
+	/* justify-content: center; */
+	position: fixed;
+	text-align: center;
+	transform: rotate(90deg);
+	white-space: nowrap;
+	h3 {
+		padding: 2px 4px;
+		margin: 0;
+		position: absolute;
+		right: 0;
+		background-color: #0000ff;
+		color: #fff;
+	}
+`;
 
 function App() {
-  const [isClicked, setClicked] = useState(false);
+	const [isClicked, setClicked] = useState(false);
 
-  return (
-    <div className="App">
-      <StyledWrapper onClick={() => setClicked(!isClicked)}>
-        <h3>Web developer</h3>
-      </StyledWrapper>
-      <Modal isActive={isClicked} />
-      <FrontSection isActive={isClicked} />
-      <SecondSection />
-    </div>
-  );
+	return (
+		<div className="App">
+			<StyledWrapper onClick={() => setClicked(!isClicked)}>
+				<h3>Web developer</h3>
+			</StyledWrapper>
+			<Modal isActive={isClicked} />
+			<FrontSection isActive={isClicked} />
+			<SecondSection />
+		</div>
+	);
 }
 
 export default App;
--- src/components/Blob/index.js
+++ src/components/Blob/index.js
@@ -1,32 +1,39 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { TweenLite } from "gsap";
+import { TweenLite } from 'gsap/dist/gsap';
 
 import styled from 'styled-components';
 
 const StyledCircle = styled.circle`
-    /* background: black; */
-
+	/* background: black; */
 `;
 
-
 const Blob = (props) => {
-    const circle = useRef(null);
+	const circle = useRef(null);
 
+	useEffect(() => {
+		moveMe(circle.current);
+	}, []);
 
-    useEffect(() => {
-        moveMe(circle.current);
-    }, []);
+	const getRandomIntBetween = (from, to) => {
+		return Math.floor(Math.random() * (to + 1 - from)) + from;
+	};
 
-    const getRandomIntBetween = (from, to) => {
-        return (Math.floor(Math.random() * ((to + 1) - from)) + from)
-    }
-
-    const moveMe = (target) => {
-        TweenLite.to(target, Math.random() * 6, { x: Math.random() * 300, y: Math.random() * 300, onComplete: moveMe, onCompleteParams: [target] })
-    }
-    return (
-        <StyledCircle ref={circle} cx={getRandomIntBetween(0, window.innerWidth / 2)} cy={getRandomIntBetween(0, window.innerHeight / 2)} r={getRandomIntBetween(25, 100)} fill="black" />
-
-    )
-}
-export default Blob
+	const moveMe = (target) => {
+		TweenLite.to(target, Math.random() * 6, {
+			x: Math.random() * 300,
+			y: Math.random() * 300,
+			onComplete: moveMe,
+			onCompleteParams: [target],
+		});
+	};
+	return (
+		<StyledCircle
+			ref={circle}
+			cx={getRandomIntBetween(0, window.innerWidth / 2)}
+			cy={getRandomIntBetween(0, window.innerHeight / 2)}
+			r={getRandomIntBetween(25, 100)}
+			fill="black"
+		/>
+	);
+};
+export default Blob;
